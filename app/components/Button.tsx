0ce0a5d0fe53827294e7b4332433a6e8e230b5f9
petsamuel <petsamuel4@gmail.com>
2025-01-09 05:14:54 +0100

    ✨ feat: enhance Hero section with new layout and Button component; update global styles and configurations

---
+++ app/components/Button.tsx
@@ -0,0 +1,16 @@
+import { stringify } from "querystring";
+import React from "react";
+
+type buttonProps = {
+  text: string;
+  className?: string;
+  href?: string;
+};
+
+export const Button = ({ text, className, href }: buttonProps) => {
+  return (
+    <button className={className}>
+      <a href={href}>{text}</a>
+    </button>
+  );
+};
